7dd6181a5462b1ebd2b9d90ef515705cbd17f6bf
Blake VandeMerwe <blakev@email.com>
2017-10-31 13:26:08 -0600

    Add Firefox Dockerfile and configurations

---
+++ dockerfiles/standalone-firefox-ffmpeg/entry_point.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source /opt/bin/functions.sh
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+function shutdown {
+  kill -s SIGTERM $NODE_PID
+  wait $NODE_PID
+}
+
+if [ ! -z "$SE_OPTS" ]; then
+  echo "appending selenium options: ${SE_OPTS}"
+fi
+
+SERVERNUM=$(get_server_num)
+xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" \
+  java ${JAVA_OPTS} -jar /opt/selenium/selenium-server-standalone.jar \
+  ${SE_OPTS} &
+NODE_PID=$!
+
+trap shutdown SIGTERM SIGINT
+
+ratpoison &
+unclutter -idle 0.1 -root &
+
+if [ "$LIVE_STREAM" = "true" ]; then
+    ffserver -f /opt/ffserver.conf &
+fi
+
+wait $NODE_PID
