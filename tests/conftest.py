83e015a2c91076acaabe4edbcad597521b001bf6
Blake VandeMerwe <blakev@email.com>
2017-11-01 15:47:12 -0600

    Add fixture for our container factory

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,11 @@
 #     vivint-selenium-docker, 2017
 # <<
 
+import pytest
+
+from selenium_docker.base import ContainerFactory
 
 
-
+@pytest.fixture(scope='module')
+def factory():
+    return ContainerFactory.get_default_factory('unittests')
